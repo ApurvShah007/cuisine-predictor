808fbc4a84acea2ce093a81d0b6730ad8e44cb50
Anand Murali <amurali@umass.edu>
2021-12-01 18:22:17 -0500

    added logistic regression model for sent. analysis

---
+++ Code/LR_classifier.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+
+def load_data():
+    """
+    Function which loads the yelp dataset.
+
+    Returns:
+    X_train: The training set.
+    y_train: The training labels.
+    X_val: The validation set.
+    y_val: The validation labels.
+    X_test: The test set.
+    y_test: The test labels.
+    """
+
+    X_train = pd.read_csv("../Data/X_train.csv")["Review"].values
+    y_train = pd.read_csv("../Data/y_train.csv")["Sentiment"].astype(int).values
+    
+    X_val = pd.read_csv("../Data/X_val.csv")["Review"].values
+    y_val = pd.read_csv("../Data/y_val.csv")["Sentiment"].astype(int).values
+
+    X_test = pd.read_csv("../Data/X_test.csv")["Review"].values
+    y_test = pd.read_csv("../Data/y_test.csv")["Sentiment"].astype(int).values
+
+    # create our count vectorizer and transform our data
+    vectorizer = CountVectorizer()
+    X_train = vectorizer.fit_transform(X_train)
+    X_val = vectorizer.transform(X_val)
+    X_test = vectorizer.transform(X_test)
+
+    # return the data
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def main():
+    # load the data
+    X_train, y_train, X_val, y_val, X_test, y_test = load_data()
+
+    # perform hyperparameter tuning
+    C = np.arange(1, 6, 1)
+    best_c = -1
+    best_accuracy = float("-inf")
+    validation_accuracies = []
+
+    for c in C:
+        # create the model -- use l2 regularization
+        LR = LogisticRegression(C = c, tol = 0.01, max_iter = 1000, solver = "sag")
+
+        # fit the model
+        LR.fit(X_train, y_train)
+
+        # make predictions using the validation set
+        y_val_pred = LR.predict(X_val)
+
+        # compute the accuracy
+        validation_accuracy = accuracy_score(y_val, y_val_pred)
+
+        # check if validation accuracy is better than previous
+        if validation_accuracy > best_accuracy:
+            best_accuracy = validation_accuracy
+            best_c = c
+
+        # add the validation accuracy to the list of accuracies
+        validation_accuracies.append(validation_accuracy)
+
+    # using the best hyperparameter value c, make predictions on the test set
+    LR = LogisticRegression(C = best_c)
+    LR.fit(X_train, y_train)
+    y_test_pred = LR.predict(X_test)
+    test_accuracy = accuracy_score(y_test, y_test_pred)
+    tn, fp, fn, tp = confusion_matrix(y_test, y_test_pred).ravel()
+    
+    # print the accuracy and the confusion matrix
+    print(f"Testing Accuracy for regularization value (C) {best_c}: {test_accuracy * 100}%")
+    print(f"Confusion Matrix:\n\tTP: {tp} FP: {fp}\n\tFN: {tn} TN: {tn}")
+
+    plt.plot(C, validation_accuracies, color="#d13636")
+    plt.xlabel("Amount of Regularization (C)")
+    plt.ylabel("Validation Accuracy")
+    plt.title("Validation Accuracy vs. Amount of Regularization (C)")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- Code/NB_classifier.py
+++ Code/NB_classifier.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 class NaiveBayes():
     def  __init__(self, alpha):
@@ -18,11 +18,22 @@ class NaiveBayes():
                                         self.POS_LABEL: 0
                                        }
 
+        # variables for the probability of each word given the review label 
         self.log_review_probabilities = {self.NEG_LABEL: None,
                                          self.POS_LABEL: None
                                         }
 
     def fit(self, X_train, y_train):
+        """
+        Function which "trains" the model given the training data. In essence,
+        this function calculates the log-class-probabilities and the log-probability
+        for each word.
+
+        Params:
+        X_train: The training data.
+        y_train: The training labels.
+        """
+
         # compute the class probabilities
         self.log_class_probabilities[self.NEG_LABEL] = np.log(np.sum(y_train == self.NEG_LABEL) / len(y_train))
         self.log_class_probabilities[self.POS_LABEL] = np.log(np.sum(y_train == self.POS_LABEL) / len(y_train))
@@ -41,42 +52,78 @@ class NaiveBayes():
         self.log_review_probabilities[self.POS_LABEL] = np.log(positive_review_word_counts / np.sum(positive_review_word_counts))
 
     def __predict(self, test_review):
+        """
+        Helper function which predicts the label for a single test instance by
+        calculating and comparing the unnormalized posterior probabilities of
+        the given instance.
+
+        Params:
+        test_reivew: The instance to make a prediction for.
+
+        Returns:
+        1 if the test_review belongs to the positive class, 0 otherwise.
+        """
+
         # calculate the posterior probability for the negative class
-        # negative_posterior = np.dot(self.log_review_probabilities[self.NEG_LABEL], test_review) + self.log_class_probabilities[self.NEG_LABEL]
         negative_posterior = (test_review @ self.log_review_probabilities[self.NEG_LABEL].T) + self.log_class_probabilities[self.NEG_LABEL]
         positive_posterior = (test_review @ self.log_review_probabilities[self.POS_LABEL].T) + self.log_class_probabilities[self.POS_LABEL]
 
         return 1 if positive_posterior > negative_posterior else 0
 
     def predict(self, X_test):
+        """
+        Function which generates a list of predictions for each test review in
+        the specified test set.
+
+        Params:
+        X_test: The test set to make predictions for.
+
+        Returns:
+        y_pred: The predictions for the specified test set.
+        """
+
         y_pred = np.array([self.__predict(test_review) for test_review in X_test])
         return y_pred
 
 def load_data():
-    X_train = pd.read_csv("../Data/yelp_dataset/X_train.csv")["Review"].values
-    y_train = pd.read_csv("../Data/yelp_dataset/y_train.csv")["Sentiment"].astype(int).values
+    """
+    Function which loads the yelp dataset.
 
-    X_val = pd.read_csv("../Data/yelp_dataset/X_val.csv")["Review"].values
-    y_val = pd.read_csv("../Data/yelp_dataset/y_val.csv")["Sentiment"].astype(int).values
+    Returns:
+    X_train: The training set.
+    y_train: The training labels.
+    X_val: The validation set.
+    y_val: The validation labels.
+    X_test: The test set.
+    y_test: The test labels.
+    """
 
-    X_test = pd.read_csv("../Data/yelp_dataset/X_test.csv")["Review"].values
-    y_test = pd.read_csv("../Data/yelp_dataset/y_test.csv")["Sentiment"].astype(int).values
+    X_train = pd.read_csv("../Data/X_train.csv")["Review"].values
+    y_train = pd.read_csv("../Data/y_train.csv")["Sentiment"].astype(int).values
 
+    X_val = pd.read_csv("../Data/X_val.csv")["Review"].values
+    y_val = pd.read_csv("../Data/y_val.csv")["Sentiment"].astype(int).values
+
+    X_test = pd.read_csv("../Data/X_test.csv")["Review"].values
+    y_test = pd.read_csv("../Data/y_test.csv")["Sentiment"].astype(int).values
+
+    # create our count vectorizer and transform our data
     vectorizer = CountVectorizer()
     X_train = vectorizer.fit_transform(X_train)
     X_val = vectorizer.transform(X_val)
     X_test = vectorizer.transform(X_test)
 
-    # return X_train, y_train, X_test, y_test
+    # return the data
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 def main():
     X_train, y_train, X_val, y_val, X_test, y_test = load_data()
 
-    alphas = np.arange(1, 25, 1)
+    # perform hyperparameter tuning using grid search
+    alphas = np.arange(1, 21, 1)
     best_alpha = -1
     best_accuracy = float("-inf")
-    accuracies = []
+    validation_accuracies = []
 
     for alpha in alphas:
         # create our model
@@ -95,17 +142,20 @@ def main():
             best_alpha = alpha
 
         # append this accuracy to the list
-        accuracies.append(validation_accuracy)
+        validation_accuracies.append(validation_accuracy)
 
-    # using the best alpha found, get the accuracy on the test set
+    # using the best alpha found, get the accuracy on the test set and confusion matrix
     NB = NaiveBayes(best_alpha)
     NB.fit(X_train, y_train)
     y_test_pred = NB.predict(X_test)
     test_accuracy = accuracy_score(y_test, y_test_pred)
-
+    tn, fp, fn, tp = confusion_matrix(y_test, y_test_pred).ravel()
+    
+    # print the accuracy and the confusion matrix
     print(f"Testing Accuracy for alpha value {best_alpha}: {test_accuracy * 100}%")
+    print(f"Confusion Matrix:\n\tTP: {tp} FP: {fp}\n\tFN: {fn} TN: {tn}")
 
-    plt.plot(alphas, accuracies)
+    plt.plot(alphas, validation_accuracies, color="#d13636")
     plt.xlabel("Alpha")
     plt.ylabel("Validation Accuracy")
     plt.title("Validation Accuracy vs. Alpha")
